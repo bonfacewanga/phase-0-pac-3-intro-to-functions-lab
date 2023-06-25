44a9c7970d5cdbb96e9812907347e1950033063a
Bonface <bonniewanga34@gmail.com>
2023-06-25 21:26:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ return string.toLowerCase()
 function logWhisper(string){
     console.log(string.toLowerCase())
 }
-let string;
+
 function sayHiToHeadphonedRoommate(string){
-if(string===string.toLowerCase){
+if(string===string.toLowerCase()){
     return "I can't hear you!"
-}else if(string===string.toUpperCase){
+}else if(string===string.toUpperCase()){
     return "YES INDEED!"
 }else if(string==="Let's have dinner together!"){
     return "I would love to!"
 }
-return string;
+else{
+  return "Response not found!"
+}
 }
